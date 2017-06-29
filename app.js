3ecc31fefbb08bd294f158dfe49d32cc19b8d70e
DarkTachyon <tmb.alexander@gmail.com>
2017-06-29 13:48:54 -0400

    Make it look nice

--- app.js
+++ app.js
@@ -5,14 +5,71 @@ const app = {
         this.list = document.querySelector(selectors.listSelector)
 
         document
-          .querySelector(selectors.formSelector)
-          .addEventListener('submit', this.handleSubmit.bind(this))
+            .querySelector(selectors.formSelector)
+            .addEventListener('submit', this.handleSubmit.bind(this))
       },
 
     renderListItem(flick) {
-        const item = document.createElement('li')
-        item.textContent = flick.name
-        return item
+        const listDiv = document.createElement('div')
+        listDiv.setAttribute('class', 'grid-x')
+        listDiv.style.backgroundColor = '#fafafa'
+
+        const flickDiv = document.createElement('div')
+        flickDiv.setAttribute('class', 'small-6 cell')
+
+        const words = document.createElement('p')
+        words.textContent = flick.name
+
+        const buttonsDiv = document.createElement('div')
+        buttonsDiv.setAttribute('class', 'small-6 cell')
+
+        const placeDiv = document.createElement('div')
+        placeDiv.setAttribute('class', 'medium-8 medium-offset-2 cell')
+
+        const buttonGroupDiv = document.createElement('div')
+        buttonGroupDiv.setAttribute('class', 'button-group')
+
+        const favButton = document.createElement('button')
+        favButton.setAttribute('class', 'hollow button warning')
+        favButton.textContent = 'Favorite'
+        favButton.addEventListener('favorite', this.handleFav.bind(this))
+
+        const upButton = document.createElement('button')
+        upButton.setAttribute('class', 'hollow button secondary')
+        upButton.textContent = 'Up'
+        upButton.addEventListener('up', this.handleUp.bind(this))
+
+        const downButton = document.createElement('button')
+        downButton.setAttribute('class', 'hollow button secondary')
+        downButton.textContent = 'Down'
+        downButton.addEventListener('down', this.handleDown.bind(this))
+
+        const delButton = document.createElement('button')
+        delButton.setAttribute('class', 'hollow button alert')
+        delButton.textContent = 'Delete'
+        delButton.addEventListener('delete', this.handleDelete.bind(this))
+
+        listDiv.appendChild(flickDiv)
+        listDiv.appendChild(buttonsDiv)
+
+        flickDiv.appendChild(words)
+
+        buttonsDiv.appendChild(placeDiv)
+
+        placeDiv.appendChild(buttonGroupDiv)
+
+        buttonGroupDiv.appendChild(favButton)
+        buttonGroupDiv.appendChild(upButton)
+        buttonGroupDiv.appendChild(downButton)
+        buttonGroupDiv.appendChild(delButton)
+
+        this.addFlick(flick)
+
+        return listDiv
+    },
+
+    addFlick(flick) {
+        this.flicks.push(flick)
     },
 
     handleSubmit(ev) {
@@ -27,7 +84,28 @@ const app = {
         this.list.appendChild(listItem)
 
         this.max++
-      },
+    },
+
+    handleFav(ev) {
+        ev.preventDefault()
+        const tar = ev.target
+        if (ev.style.backgroundColor !== '#fff099')
+            ev.style.backgroundColor = 'fafafa'
+        else
+            ev.style.backgroundColor = '#fff099'
+    },
+
+    handleUp(ev) {
+
+    },
+
+    handleDown(ev) {
+
+    },
+
+    handleDelete(ev) {
+
+    },
 }
 
 app.init({
