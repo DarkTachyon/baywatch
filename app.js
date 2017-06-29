b528234e809e67103e67a10a069f66ca6200bbe9
DarkTachyon <tmb.alexander@gmail.com>
2017-06-29 12:17:23 -0400

    Returned to previous point (before loss)

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const app = {
-    init: function() {
+    init(selectors) {
+        this.flicks = []
+        this.max = 0
+        this.list = document.querySelector(selectors.listSelector)
+
         document
-            .querySelector('form#flick-form')
-            .addEventListener('submit', this.handleSubmit)
+          .querySelector(selectors.formSelector)
+          .addEventListener('submit', this.handleSubmit.bind(this))
+      },
+
+    renderListItem(flick) {
+        const item = document.createElement('li')
+        item.textContent = flick.name
+        return item
     },
 
-    handleSubmit: function(ev) {
+    handleSubmit(ev) {
         ev.preventDefault()
         const f = ev.target
-        console.log(f.flickName.value)
-    },
+        const flick = {
+            id: this.max + 1,
+            name: f.flickName.value,
+        }
+
+        const listItem = this.renderListItem(flick)
+        this.list.appendChild(listItem)
+
+        this.max++
+      },
 }
 
-app.init()
+app.init({
+    formSelector: 'form#flick-form',
+    listSelector: '#flick-list',
+    })
